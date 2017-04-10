1ec2971298fe30f30ee18dbcac28ca5bfe5125e1
Xha Mbuwandong <host-pc@host.home>
2017-04-10 18:04:15 -0300

    Alteração na passagem de parametro por referência à função inicializar.

--- aula/arrayPontReferencia.cpp
+++ aula/arrayPontReferencia.cpp
@@ -2,17 +2,27 @@
 #include <stdlib.h>
 
 
+
+void inicializarArrayReferencia (int * v) {
+	int i;
+	for (i = 0; i < 10; i++)
+		v[i] = (i + 1) * 4;	
+}
+
+
 main (void) {
 	int i;
 	int * v;
 
 	v = (int*) malloc (sizeof (int) * 10);
 
-	for (i = 0; i < 10; i++)
-		v[i] = i+1;
+	inicializarArrayReferencia (v);
 
 	for (i = 0; i < 10; i++)
 		printf ("%d ", v[i]);
+
 	printf("\n");
+
+	
 
 }
